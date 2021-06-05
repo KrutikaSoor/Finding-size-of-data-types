d459d05384f4de929fd333a04e609af144533831
Krutika <82581947+KrutikaSoor@users.noreply.github.com>
2021-06-05 21:55:36 +0530

    Created program to enter size of data type.
    We will know the size of char,  int  , float , and double using this program.

---
+++ Sizedatatype.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"Size of char:"<< sizeof(char) << "byte";
+  cout<< "Size of int:" << sizeof(int)<< "byte";
+  cout<< "Size of float:" << sizeof(float)<< "byte";
+  cout<< "Size of double:" << sizeof(double)<< "byte";
+  return 0;
+}
